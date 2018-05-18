1e9b489e56e6dbb24a7001f6aeaf5cbc11cd4779
pmrvic <mrvic.predrag@gmail.com>
2018-05-18 19:55:18 +0200

    zupanija seeder i factory

--- app/Zupanija.php
+++ app/Zupanija.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * @author Mrvic
+ */
 class Zupanija extends Model
 {
-    //
+  // nepotrebno ako oristimo php artisan make:model Zupanija -m  
+  protected $table = 'zupanijas';  
+
 }
